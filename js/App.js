aeb0cd6c4b7438d66ff2febe5887964bf9584986
Robert Casanova <robb.casanova@gmail.com>
2012-06-12 00:40:14 +0200

    Backbone Logic

--- js/App.js
+++ js/App.js
@@ -1,26 +1,46 @@
 (function($){
-	var ABmenu = {}
-		ABmenu.View = {},
-		ABmenu.Model = {},
-		ABmenu.Collection = {};
+	var ABmenu = {
+		init: function() {
 
-	ABmenu.mainRoute = Backbone.Router.extend({
+			new ABmenu.Router.mainRoute();
+	 		Backbone.history.start();
+
+		},
+		View : {},
+		Model : {},
+		Collection : {},
+		Router : {}
+	}
+	
+
+	ABmenu.Router.mainRoute = Backbone.Router.extend({
 		  routes: {
 		    "login": "login",
 		    "logout" : "logout",
-		    "venues": "venues"    
+		    "ristoranti": "ristoranti",
+		  },
+
+		  initialize: function() {
+
+		  	this.login  = new ABmenu.View.login();
+		  	this.logout = new ABmenu.View.logout();
+		  	this.ristoranti = new ABmenu.View.ristoranti();
+		  	//
 		  },
 
 		  login: function() {
-		  	new ABmenu.View.login();
+
+		  	$('.main').html(this.login.render().el);
+
 		  },
 		  logout: function() {
-		  	new ABmenu.View.logout();
-		  },
-		  venues: function() {
 
-			this.venues = new ABmenu.Collection.venues();
-			this.venues_view = new ABmenu.View.venues({collection: this.venues});
+		  	$('.main').html(this.logout.render().el);
+
+		  },
+		  ristoranti: function() {
+
+			$('.main').html(this.ristoranti.render().el);
 		  			  	
 		  }
 
@@ -28,49 +48,91 @@
 
 	/* VIEWS */
 	ABmenu.View.login = Backbone.View.extend({
-		el: $('.main'),
+		tagName: 'div',
+		className: 'login',
+		template: _.template($('#tmpl-login').html()),
 		initialize: function(){
-			this.template = _.template($('#tmpl-login').html());
-			this.render();
+
 		},
 		render: function() {
 			var data = {};
 			data.title="Login";
-			$(this.el).html(this.template(data));
+			this.$el.html(this.template(data));
+
     		return this;
 		}
 
 	})
 	ABmenu.View.logout = Backbone.View.extend({
-		el: $('.main'),
+		tagName: 'div',
+		className: 'logout',
+		template: _.template($('#tmpl-logout').html()),
 		initialize: function(){
-			this.render();
+
 		},
 		render: function() {
-			this.$el.html("<h1>Logout</h1>");
+			this.$el.html(this.template());
     		return this;
 		}
 
 	})
-	ABmenu.View.venues = Backbone.View.extend({
-		el: $('.main'),
-		initialize: function(){
-			var that = this;
-			this.collection.fetch();
-			this.collection.on("reset", this.render, this);
+	ABmenu.View.ristoranti = Backbone.View.extend({
+		tagName: 'div',
+		className: 'ristoranti',
+		events: {
+			'click .load-foursquare' : 'loadFoursquare',
+			'click .venues li' : 'changeTitle'
 		},
-		render: function(collection) {
-			var $cached = $('<ul>');
-			_.each(collection.models,function(venue){
-				$cached.append(new ABmenu.View.venue({model: venue}).render().el);
+		template: _.template($('#tmpl-ristoranti').html()),
+		initialize: function(){
+		  	this.venues = new ABmenu.Collection.venues();
+			this.venues_view = new ABmenu.View.venues({collection: this.venues});
+			this.venues.fetch();
+		},
+		render: function() {
+			this.$el.html(this.template());
+			this.$el.find('.list').append(this.venues_view.render().el);
+    		return this;
+		},
+		loadFoursquare: function(e) {
+			e.preventDefault();
+			this.venues.url = e.currentTarget.href;
+			this.venues.fetch();
+
+		},
+		changeTitle: function(e) {
+			var venue = this.venues.find(function(venue) {
+					return venue.get('id') == $(e.currentTarget).data('id')
+			});
+			venue.set({name:'It works'});
+			venue.save();
+		}
+
+	})
+	 
+
+	ABmenu.View.venues = Backbone.View.extend({ //view collegate al model
+		tagName: 'ul',
+		className: 'venues',
+		initialize: function(){
+			_.bindAll(this, 'render');
+			this.collection.on('reset',this.render);
+		},
+		render: function() {
+			this.$el.empty();
+			_.each(this.collection.models,function(venue){
+				this.$el.append(new ABmenu.View.venue({model: venue}).render().$el);
 			},this);
-			this.$el.html($cached);
     		return this;
 		}
 	});
-	ABmenu.View.venue = Backbone.View.extend({
+	ABmenu.View.venue = Backbone.View.extend({ //view collegate al model
 		tagName: 'li',
 		template: _.template($('#tmpl-venue').html()),
+		initialize: function(){
+			_.bindAll(this, 'render');
+      		this.model.on('change', this.render);
+		},
 		render: function() {
 			this.$el.html(this.template(this.model.toJSON()));
 			return this;
@@ -89,14 +151,15 @@
 	})
 	ABmenu.Collection.venues = Backbone.Collection.extend({
 		model: ABmenu.Model.venue,
-		url: 'api/venues.json'
+		url: 'api/venues.json',
+		initialize: function() {
+		},
 	})
 
 
 
 	$(document).ready(function(){
-	 	new ABmenu.mainRoute();
-	 	Backbone.history.start();
+	 	ABmenu.init();
 
 	})		
 
